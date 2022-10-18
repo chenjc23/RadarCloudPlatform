6345777dd682abd50bb683b2bcda3d0bea067c9b
chenjc23 <chenjc23@mail2.sysu.edu.cn>
2022-10-18 23:02:32 +0800

    增加数据分析图表组件

---
+++ src/pages/DataAnalyse/components/DataAnalyseCharts.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Line } from "@ant-design/charts";
+import { Radio } from "antd";
+import { ProCard } from "@ant-design/pro-components";
+
+const DataAnalyseCharts: React.FC = () => {
+
+  const [data, setData] = useState([]);
+
+  const dataFetch = () => {
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json')
+      .then((res) => res.json())
+      .then((dataJson) => setData(dataJson))
+      .catch((e) => {
+        console.error('fetch data failed', e);
+      });
+  };
+
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+  const config = {
+    data,
+    title: '形变曲线(mm)',
+    xField: 'year',
+    yField: 'value',
+    seriesField: 'category',
+    xAxis: {
+      type: 'time',
+    },
+    yAxis: {
+      label: {}
+    },
+  };
+
+  return (
+    <div>
+      <ProCard title='形变曲线(mm)' bordered={true}>
+        {/*<Radio.Group defaultValue={1}>*/}
+        {/*  <Radio value={1}>形变曲线(mm)</Radio>*/}
+        {/*  <Radio value={2}>速度曲线(mm/h)</Radio>*/}
+        {/*  <Radio value={3}>加速度曲线(mm/h²)</Radio>*/}
+        {/*</Radio.Group>*/}
+        <Line
+          height={300}
+          {...config}
+        />
+      </ProCard>
+      <ProCard title='速度曲线(mm/h)' bordered={true}>
+        <Line
+          height={300}
+          {...config}
+        />
+      </ProCard>
+      <ProCard title='加速度曲线(mm/h²)' bordered={true}>
+        <Line
+          height={300}
+          {...config}
+        />
+      </ProCard>
+    </div>
+  );
+};
+
+export default DataAnalyseCharts;
