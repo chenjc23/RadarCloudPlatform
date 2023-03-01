679a912b71d1586afda911b63861cd2a69f4fda5
chenjc23 <chenjc23@mail2.sysu.edu.cn>
2023-03-01 20:47:00 +0800

    refactor: radarServer

--- server/RadarServer/RadarDataSocket.js
+++ server/RadarServer/RadarDataSocket.js
@@ -15,48 +15,50 @@ class RadarDataSocket {
   }
 
   // 向雷达发送连接请求
-  connectRadar() {
-    this.socket.connect(
-      {
+  async connectRadar() {
+    // 监测socket是否已连接
+    if (this.socket.readyState === 'open' || this.socket.readyState === 'writeOnly')
+      return Promise.resolve();
+    // 若未连接则发起连接
+    return new Promise((resolve) => {
+      this.socket.connect(
         // 设置雷达网络IP和端口
-        host,
-        port,
-      },
-      () => {
-        console.log(`===与雷达-->${host}:${port} 的TCP连接已建立`);
-      },
-    );
+        { host, port },
+        () => {
+          console.log(`===与雷达-->${host}:${port} 的TCP连接已建立`);
+          resolve();
+        },
+      );
+    });
   }
 
   // 设置雷达参数
   async setWorkParams(buf) {
-    await new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       this.socket.write(buf, () => {
-        if (this.isOpen()) {
-          console.log(`===已成功向雷达发送参数设置指令：${buf}`);
-          resolve();
-        } else reject('雷达参数设置失败');
+        console.log(`===已成功向雷达发送参数设置指令：${buf}`);
+        resolve();
       });
-      setTimeout(() => reject('雷达参数设置失败'), 2000);
     });
   }
 
+  // 设置雷达参数(待更新版本)
+  // async setWorkParams2(buf) {
+  // }
+
   // 发送采集指令
   async staticReadOrder(buf) {
-    return await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       this.socket.write(buf, () => {
-        if (this.isOpen()) {
-          console.log(`===已成功向雷达发送静止采集指令：${buf}`);
-          // 构造文件输出流
-          // const fileName = `radarData${newFileId}.bin`;
-          // const binOutputFlow = fs.createWriteStream(fileName, {flags: 'a'});
-          // // 利用nodejs的pipe功能，直接将socket输入流与文件输出流对接（便捷至极）
-          // this.socket.pipe(binOutputFlow);
-          // console.log(`===采集数据保存的目标文件：${fileName}`);
-          resolve(true);
-        } else reject('雷达静止采集命令发送失败');
+        console.log(`===已成功向雷达发送静止采集指令：${buf}`);
+        resolve();
+        // 构造文件输出流
+        // const fileName = `radarData${newFileId}.bin`;
+        // const binOutputFlow = fs.createWriteStream(fileName, {flags: 'a'});
+        // // 利用nodejs的pipe功能，直接将socket输入流与文件输出流对接（便捷至极）
+        // this.socket.pipe(binOutputFlow);
+        // console.log(`===采集数据保存的目标文件：${fileName}`);
       });
-      setTimeout(() => reject('雷达静止采集命令发送失败'), 2000);
     });
   }
 
